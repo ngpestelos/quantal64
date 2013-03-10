258db140a77e3a4a411bce84459eac948d0de5db
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-03-10 13:18:40 +0800

    replace postinstall.sh with base.sh

--- definition.rb
+++ definition.rb
@@ -30,6 +30,8 @@ Veewee::Session.declare({
   :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
   :shutdown_cmd => "shutdown -P now",
-  :postinstall_files => [ "postinstall.sh"],
+  :postinstall_files => [
+    "base.sh"
+  ],
   :postinstall_timeout => "10000"
 })
