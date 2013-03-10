badac92845cf0ec3d9b15558fd0db5e7f9e4770d
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-03-10 13:16:19 +0800

    add definition and base post-install script

---
+++ base.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Update timestamp 
+date > /etc/vagrant_box_build_time
+
+# Fix locale bug
+dpkg-reconfigure locales
+echo "LC_ALL=en_US.UTF-8" >> /etc/defaults/locale
+
+# Set up sudo
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+echo <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US.UTF-8"
+EOF
+
+update-grub
+
+exit
