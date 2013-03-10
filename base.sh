a26798a42d50b1946136a4427d25ca27b25e85c7
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-03-10 15:17:20 +0800

    install vagrant keys

--- base.sh
+++ base.sh
@@ -28,4 +28,12 @@ EOF
 
 update-grub
 
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
 exit
