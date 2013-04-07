fba7c95b12a115c65a2e5354054fa73a240a92b2
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-04-07 11:43:39 +0800

    add ruby and chef

--- postinstall.sh
+++ postinstall.sh
@@ -38,6 +38,35 @@ wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/key
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
+RUBY_VERSION="1.9.3-p392"
+RUBY=/opt/ruby/bin/ruby
+GEM=/opt/ruby/bin/gem
+RUBYGEMS_VERSION="1.8.25"
+
+cd /tmp
+
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-$RUBY_VERSION.tar.gz
+tar xvzf ruby-$RUBY_VERSION.tar.gz
+cd ruby-$RUBY_VERSION
+./configure CFLAGS="-O3 -Wall" --prefix=/opt/ruby
+make
+make install
+cd ..
+rm -rf ruby-$RUBY_VERSION
+rm ruby-$RUBY_VERSION.tar.gz
+
+wget http://production.cf.rubygems.org/rubygems/rubygems-$RUBYGEMS_VERSION.tgz
+tar xzf rubygems-$RUBYGEMS_VERSION.tgz
+cd rubygems-$RUBYGEMS_VERSION
+$RUBY setup.rb
+cd ..
+rm -rf rubygems-$RUBYGEMS_VERSION
+rm rubygems-$RUBYGEMS_VERSION.tgz
+
+$GEM install chef --no-ri --no-rdoc
+mkdir -p /etc/chef
+echo 'PATH=$PATH:/opt/ruby/bin/' > /etc/profile.d/vagrantruby.sh
+
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
