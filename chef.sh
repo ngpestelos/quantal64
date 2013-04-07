cbef44654a1e1e12eabe28e6fbd3e566b6a502ec
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-04-07 09:33:05 +0800

    fix reference errors

--- chef.sh
+++ chef.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-
 # Based on http://goo.gl/gzilp
+
+RUBY_VERSION="1.9.3-p392"
+RUBY=/opt/ruby/bin/ruby
+GEM=/opt/ruby/bin/gem
+RUBYGEMS_VERSION="1.8.25"
 
 cd /tmp
 
-# Install Ruby 1.9.3 from source in /opt
-$RUBY_VERSION="1.9.3-p392"
 wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-$RUBY_VERSION.tar.gz
-tar xvzf $RUBY_VERSION
-cd $RUBY_VERSION
+tar xvzf ruby-$RUBY_VERSION
+cd ruby-$RUBY_VERSION
 ./configure CFLAGS="-O3 -Wall" --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf $RUBY_VERSION
+rm -rf ruby-$RUBY_VERSION
 rm $RUBY_VERSION.tar.gz
-
-RUBY=/opt/ruby/bin/ruby
-GEM=/opt/ruby/bin/gem
-RUBYGEMS_VERSION="1.8.25"
 
 wget http://production.cf.rubygems.org/rubygems/rubygems-$RUBYGEMS_VERSION.tgz
 tar xzf rubygems-$RUBYGEMS_VERSION.tgz
@@ -28,6 +26,5 @@ cd ..
 rm -rf rubygems-$RUBYGEMS_VERSION
 rm rubygems-$RUBYGEMS_VERSION.tgz
 
-# Install chef
 $GEM install chef --no-ri --no-rdoc
 mkdir -p /etc/chef
