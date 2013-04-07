e4914f5dcc390cc99386ef20b63001f823335777
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-04-07 11:27:11 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install linux-headers-$(uname -r) 
+apt-get -y install linux-headers-$(uname -r)
 apt-get -y install build-essential
 apt-get -y install zlib1g-dev
 apt-get -y install libssl-dev
@@ -15,13 +15,5 @@ apt-get -y install libsqlite3-dev
 apt-get -y install libv8-dev
 apt-get -y install libpq-dev
 apt-get -y install libxslt-dev
-apt-get -y install vim
-apt-get -y install tmux
-apt-get -y install git-core
-apt-get -y install curl
-apt-get -y install unzip
-apt-get -y install autoconf
-apt-get -y install ack
-apt-get -y install nfs-common
 
 apt-get clean
